73f1c43b66f8a015dc421aa8d77a0415c5c87f9f
Kalana Gunaweera <kalanagunaweera94@gmail.com>
2023-10-28 13:24:50 +0530

    junit test for save data

--- src/test/java/com/Spring/Practice/SpringbootPracticeApplicationTests.java
+++ src/test/java/com/Spring/Practice/SpringbootPracticeApplicationTests.java
@@ -1,13 +1,30 @@
 package com.Spring.Practice;
 
+import com.Spring.Practice.entity.Student;
+import com.Spring.Practice.repository.StudentRepository;
+import com.Spring.Practice.service.StudentService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootPracticeApplicationTests {
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Autowired
+	private StudentService studentService;
 
 	@Test
-	void contextLoads() {
+	void testCreateStudent() {
+		Student student = new Student();
+
+		student.setName("Kalana");
+		student.setCourse("Engineering");
+		student.setFee(300d);
+
+
+		studentRepository.save(student);
 	}
 
 }
